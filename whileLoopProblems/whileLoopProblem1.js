7d79b324076096f303771ac4727b6e1d2b70c843
Vishal Garg <vishugarg10004@gmail.com>
2020-11-15 23:41:39 +0530

    [Vishal] adding while loop code

---
+++ whileLoopProblems/whileLoopProblem1.js
@@ -0,0 +1,38 @@
+//defining console read write module for taking input from the user
+io= require('console-read-write');
+//making the function and declaring it async
+//async makes the function execute when called and wait for user input statement
+//if async is not defined then await can not be defined and all user inputs will be taken together
+async function main()
+{
+    console.log("Please enter the no. below which power for 2 is needed to be find out");
+    //taking input from the user
+    let n= await io.read(parseInt());
+    //iterating to calculate power and writing into file
+    let i=0;
+    let x=0;
+    //while loop to check two conditions
+   while(i<n && power(i)<=256)
+    {
+         x= power(i);
+        console.log("Element at "+(i+1)+"th position is: "+x);
+        i++;
+    }
+}
+//function to calculate power
+function power(a)
+{
+    let powerResult=0;
+    //loop to iterate upto a and calculate power
+    for(let j=0;j<=a;j++)
+    {
+        if(j==0)
+        {
+            powerResult=1;
+        }
+        else
+            powerResult= 2*powerResult;
+    }
+    return powerResult;
+}
+main();
